ebcf757c4744d850cb0eee3dac4c6a34fe72293e
Emily Costa <ecost020@fiu.edu>
2020-04-26 21:28:27 -0400

    began project

---
+++ src/main/java/com/mycompany/midtermproject/Exposed.java
@@ -0,0 +1,30 @@
+/*
+ * @author Federico Manzo
+ * For BTE324 Midterm Exam
+ * This project takes an input file containing patients at a hospital
+ * and outputs a file containing the patients that potentially have
+ * COVID-19. The functions will rate the patients on their likelihood 
+ * of having the virus, and the doctors/nurses/patients that potentially came
+ * in contact with them.
+ */
+
+package com.mycompany.midtermproject;
+
+/**
+ *
+ * @author emily
+ */
+public class Exposed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+        
+    // Function that creates patient objects from input file.
+    // Include booleans for symptoms (ex. true/false for cough)
+    // First/last name, 
+  
+}
